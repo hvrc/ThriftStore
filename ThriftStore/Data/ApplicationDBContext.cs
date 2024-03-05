4aba2c55a4cf450d7eb8bfc46355fd2a8d78fe20
hvrc <harshrajmachikar@gmail.com>
2024-03-04 23:40:30 -0500

    first project increment, mock functionality

---
+++ ThriftStore/Data/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ThriftStore.Models;
+
+namespace ThriftStore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // Represents the collection of entities in the database
+        public DbSet<User> Users { get; set; }
+        public DbSet<Listing> Listings { get; set; }
+
+        // Constructor that takes DbContextOptions as a parameter
+        // Automatically applies any pending migrations and creates the database if needed
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            Database.Migrate();
+        }
+    }
+}
--- ThriftStore/Program.cs
+++ ThriftStore/Program.cs
@@ -1,15 +1,32 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Logging;
+using ThriftStore.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+builder.Configuration.AddConfiguration(configuration);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -22,8 +39,11 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Listing}/{action=ListingIndex}/{id?}");
+
+app.MapControllerRoute(
+    name: "userDelete",
+    pattern: "User/Delete/{id?}",
+    defaults: new { controller = "User", action = "UserDelete" });
 
 app.Run();
-
-//
